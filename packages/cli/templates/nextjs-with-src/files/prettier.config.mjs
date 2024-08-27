57368de29c0ea101c2bd43816d2b91cfaad35beb
Rifat Khan <rifat6633@gmail.com>
2024-08-27 20:27:49 +0600

    feature: add template files for a nextjs project

---
+++ packages/cli/templates/nextjs-with-src/files/prettier.config.mjs
@@ -0,0 +1,34 @@
+/** @type {import('prettier').Config} */
+const prettierConfig = {
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^types$",
+    "^@/types/(.*)$",
+    "^@/config/(.*)$",
+    "^@/lib/(.*)$",
+    "^@/hooks/(.*)$",
+    "^@/components/ui/(.*)$",
+    "^@/components/(.*)$",
+    "^@/app/(.*)$",
+    "",
+    "^[./]",
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+};
+
+export default prettierConfig;
---
+++ packages/cli/templates/nextjs-with-src/files/src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <main className="p-10 text-center text-2xl font-semibold">Hello</main>;
+}
