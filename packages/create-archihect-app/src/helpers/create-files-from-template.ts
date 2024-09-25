79443aeb3e7706af1beff29bce83540be13d9943
Rifat Khan <rifat6633@gmail.com>
2024-09-25 16:23:02 +0600

    refactor: build a createFilesFromTemplate function to write or copy files from template dir

---
+++ packages/create-archihect-app/src/helpers/create-files-from-template.ts
@@ -0,0 +1,44 @@
+import fg from "fast-glob";
+import fs from "fs-extra";
+import path from "path";
+
+/**
+ * Creates files and directory from a template directory.
+ * @param templatePath Path of the template directory.
+ * @param projectPath Path of the project directory.
+ * @param ignore Array of patterns to ignore files.
+ */
+export default async function createFilesFromTemplate(
+  templatePath: string,
+  projectPath: string,
+  ignore: string[] = [],
+) {
+  const templateFiles = await fg.glob("**/*", {
+    cwd: templatePath,
+    ignore: ignore,
+    dot: true,
+  });
+
+  templateFiles.forEach(async (file) => {
+    const templateFilePath = path.join(templatePath, file);
+
+    const content = await fs.readFile(templateFilePath);
+
+    const fileName = () => {
+      switch (file) {
+        case ".git-ignore":
+          return ".gitignore";
+        default:
+          return file;
+      }
+    };
+
+    await fs.mkdir(path.dirname(path.join(projectPath, fileName())), {
+      recursive: true,
+    });
+
+    /\.(tsx|ts|css)$/.test(file)
+      ? await fs.writeFile(path.join(projectPath, fileName()), content)
+      : await fs.copyFile(templateFilePath, path.join(projectPath, fileName()));
+  });
+}
--- packages/create-archihect-app/src/main/create-project.ts
+++ packages/create-archihect-app/src/main/create-project.ts
@@ -1,15 +1,14 @@
-import { execa } from "execa";
 import path from "path";
 import fs from "fs-extra";
 import { directories } from "@/utils/constants.js";
-import fg from "fast-glob";
 import createPackageJson from "@/helpers/create-package-json.js";
+import createFilesFromTemplate from "@/helpers/create-files-from-template.js";
 
 export default async function createProject(projectName: string) {
   // Create project directory
   const projectPath = path.join(directories.CURRENT_DIR, projectName);
   if (!fs.pathExists(projectPath)) {
-    await execa`mkdir ${projectPath}`;
+    await fs.mkdir(projectPath);
   } else {
     await fs.emptyDir(projectPath);
   }
@@ -18,54 +17,16 @@ export default async function createProject(projectName: string) {
   await createPackageJson(projectName, projectPath);
 
   // Create base files for Nextjs
-  const baseFilesToCreate = await fs.readdir(
-    path.join(directories.TEMPLATE_DIR, "nextjs/base"),
+  await createFilesFromTemplate(
+    path.join(directories.NEXT_TEMPLATE_DIR, "base"),
+    projectPath,
   );
 
-  baseFilesToCreate.forEach(async (file) => {
-    const baseFilePath = path.join(
-      directories.TEMPLATE_DIR,
-      "nextjs/base",
-      file,
-    );
-
-    const fileContent = await fs.readFile(baseFilePath, "utf8");
-
-    if (file === ".git-ignore") {
-      await fs.writeFile(`${projectPath}/.gitignore`, fileContent, "utf8");
-    } else {
-      await fs.writeFile(`${projectPath}/${file}`, fileContent, "utf8");
-    }
-  });
-
   // Create App directory files for Nextjs
-  const nextAppDir = path.join(projectPath, "app");
-
-  await execa`mkdir ${nextAppDir}`;
-
-  const appDirTemplatePath = path.join(directories.NEXT_TEMPLATE_DIR, "app");
-
-  const filesToCreate = await fg.glob("**/*", {
-    cwd: appDirTemplatePath,
-    ignore: ["**/fonts/*"],
-  });
-
-  filesToCreate.forEach(async (file) => {
-    const targetPath = path.join(appDirTemplatePath, file);
-    const content = await fs.readFile(targetPath);
-    await fs.writeFile(`${nextAppDir}/${file}`, content);
-  });
-
-  await execa`mkdir ${nextAppDir}/fonts`;
-
-  const copyfonts = await fg.glob("**/fonts/*", {
-    cwd: appDirTemplatePath,
-  });
-
-  copyfonts.forEach(async (file) => {
-    const targetPath = path.join(appDirTemplatePath, file);
-    await fs.copyFile(targetPath, `${nextAppDir}/${file}`);
-  });
+  await createFilesFromTemplate(
+    path.join(directories.NEXT_TEMPLATE_DIR, "app"),
+    path.join(projectPath, "app"),
+  );
 
   console.log("Project created successfully.");
 }
